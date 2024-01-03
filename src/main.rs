2659546212e639aae3a798f5b2d9176d3262e6a2
Justin Wylie <justinwylie@olarm.co>
2024-01-03 22:04:43 +0200

    Add to playlist working Code cleanup (removed duplicated code) Trying to get paging working for playlists with more than 100 tracks

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use chrono::{DateTime, Utc};
 use futures::{Stream, StreamExt};
 use rspotify::model::playlist::PlaylistTracksRef;
-use rspotify::model::FullPlaylist;
-use rspotify::model::FullTrack;
-use rspotify::model::PlayableItem;
-use rspotify::model::PlaylistId;
-use rspotify::model::PublicUser;
-use rspotify::model::SimplifiedPlaylist;
-use rspotify::model::UserId;
-use rspotify::ClientError;
+use rspotify::model::{
+    FullPlaylist, FullTrack, Page, PlayableItem, PlaylistId, PlaylistItem, PublicUser,
+    SimplifiedPlaylist, UserId,
+};
 use rspotify::{
-    // model::{Country, Market, SearchType},
-    prelude::*,
-    ClientCredsSpotify,
-    Credentials,
-    AuthCodeSpotify
+    prelude::*, scopes, AuthCodeSpotify, ClientCredsSpotify, ClientError, Config, Credentials,
+    OAuth,
 };
 
 // local caching to speed up development
@@ -41,12 +34,41 @@ async fn main() {
     println!("Hello, lets talk with Spotify!");
 
     // TODO: would be nice to move this to be const (global) but need to figure out how :)
-    let recent_onehundred_id: PlaylistId = PlaylistId::from_uri("spotify:playlist:5j1TO7kHW28exlT5HaBua1").unwrap();
+    let recent_onehundred_id: PlaylistId =
+        PlaylistId::from_uri("spotify:playlist:5j1TO7kHW28exlT5HaBua1").unwrap();
 
     let creds = Credentials::from_env().unwrap();
-    let spotify: ClientCredsSpotify = ClientCredsSpotify::new(creds);
+    let oauth = OAuth::from_env(scopes!(
+        "playlist-read-collaborative",
+        "playlist-modify-public"
+    ))
+    .unwrap(); // for now I am excluding private playlists "playlist-read-private"
 
-    spotify.request_token().await.unwrap();
+    // let spotify: ClientCredsSpotify = ClientCredsSpotify::new(creds); // no user specific actions available
+    // spotify.request_token().await.unwrap();
+
+    let mut config = Config::default();
+    config.token_cached = true;
+
+    let spotify = AuthCodeSpotify::with_config(creds, oauth, config);
+
+    // no token caching
+    let url = spotify.get_authorize_url(false).unwrap();
+    spotify.prompt_for_token(&url).await.unwrap();
+
+    // token caching
+    // let token_available = spotify.read_token_cache(false).await;
+    // match token_available {
+    //     Ok(token) => {
+    //         println!("cached token available: {:?}", token.unwrap());
+    //         spotify.get_oauth();
+    //     },
+    //     Err(error) => {
+    //         dbg!(error);
+    //         let url = spotify.get_authorize_url(false).unwrap();
+    //         spotify.prompt_for_token(&url).await.unwrap();
+    //     },
+    // }
 
     // collect all user tracks
     let mut all_tracks = get_all_user_tracks(&spotify).await;
@@ -89,18 +111,21 @@ async fn main() {
     //     println!("{}", track.track.as_ref().unwrap().name);
     // }
 
-    let current_recent_onehundred_tracks = get_tracks_with_playlist_id(&spotify, &recent_onehundred_id).await;
+    let current_recent_onehundred_tracks =
+        get_tracks_with_playlist_id(&spotify, &recent_onehundred_id).await;
     let mut tracks_to_remove = current_recent_onehundred_tracks.clone();
     let mut tracks_to_add = recent_onehundred.clone();
 
     remove_tracks(&mut tracks_to_remove, &recent_onehundred); // remaining in tracks_to_remove are the tracks we must ask spotify to remove from the recent 100 playlist
     remove_tracks(&mut tracks_to_add, &current_recent_onehundred_tracks); // remaining in recent_onehundred are the tracks we must ask spotify to add to the recent 100 playlist
-    
+
     println!("tracks we must remove: {}", tracks_to_remove.len());
     println!("tracks we must add: {}", tracks_to_add.len());
+
+    add_to_playlist(&spotify, &recent_onehundred_id, &tracks_to_add).await;
 }
 
-async fn user_playlists(spotify: &ClientCredsSpotify) -> Vec<SimplifiedPlaylist> {
+async fn user_playlists(spotify: &AuthCodeSpotify) -> Vec<SimplifiedPlaylist> {
     let user_playlists: std::pin::Pin<
         Box<
             dyn Stream<Item = Result<rspotify::model::SimplifiedPlaylist, rspotify::ClientError>>
@@ -125,41 +150,55 @@ async fn user_playlists(spotify: &ClientCredsSpotify) -> Vec<SimplifiedPlaylist>
 }
 
 async fn get_tracks(
-    spotify: &ClientCredsSpotify,
+    spotify: &AuthCodeSpotify,
     playlist: &SimplifiedPlaylist,
 ) -> Vec<PlaylistTrack> {
-    let playlist = spotify
-        .playlist(playlist.id.clone(), None, None)
-        .await
-        .unwrap(); // TODO: could filter for only track field
-                   // let track_list = playlist.tracks;
+    // TODO: could filter for only track field let track_list = playlist.tracks;
+    if let Ok(playlist) = spotify.playlist(playlist.id.clone(), None, None).await {
+        // TODO: need to check all pages, if there are more than 1 pages.
+        // generate a vector of playlist tracks using a custom 'closure'
+        return get_all_playable_tracks(&spotify, &playlist.tracks).await;
+    } else {
+        return Vec::<PlaylistTrack>::new();
+    }
+}
 
-    // TODO: need to check all pages, if there are more than 1 pages.
-    // generate a vector of playlist tracks using a custom 'closure'
-    let playlist_tracks: Vec<PlaylistTrack> = playlist
-        .tracks
+async fn get_all_playable_tracks(
+    spotify: &AuthCodeSpotify,
+    tracks: &Page<PlaylistItem>,
+) -> Vec<PlaylistTrack> {
+    let playlist_tracks = tracks
         .items
         .iter()
-        .map(|track_item| PlaylistTrack {
-            added_at: track_item.added_at.clone(),
-            added_by: track_item.added_by.clone(),
-            is_local: track_item.is_local,
-            track: if let Some(track) = &track_item.track {
-                match track {
+        .map(|playlist_item| {
+            PlaylistTrack {
+                added_at: playlist_item.added_at.clone(),
+                added_by: playlist_item.added_by.clone(),
+                is_local: playlist_item.is_local,
+                track: if let Some(playable_item) = &playlist_item.track {
+                    match playable_item {
                     PlayableItem::Track(track) => Some(track.clone()),
                     PlayableItem::Episode(_) => None,
-                }
-            } else {
-                None
-            },
+                    }
+                } else {
+                    None
+                },
+            }
         })
         .collect();
+
+    if let Some(next_page) = &tracks.next {
+        dbg!(next_page);
+        // let next_tracks = next_page;
+    } else {
+        println!("no next page!");
+    }
 
     return playlist_tracks;
 }
 
 async fn get_tracks_with_playlist_id(
-    spotify: &ClientCredsSpotify,
+    spotify: &AuthCodeSpotify,
     playlist_id: &PlaylistId<'_>,
 ) -> Vec<PlaylistTrack> {
     let playlist = spotify.playlist(playlist_id.clone(), None, None).await;
@@ -171,28 +210,10 @@ async fn get_tracks_with_playlist_id(
         }
     };
 
-    let tracks_from_playlist: Vec<PlaylistTrack> =
-        playlist
-            .tracks
-            .items
-            .iter()
-            .map(|playlist_item| PlaylistTrack {
-                added_at: playlist_item.added_at.clone(),
-                added_by: playlist_item.added_by.clone(),
-                is_local: playlist_item.is_local,
-                track: if let Some(track) = &playlist_item.track {
-                    match track {
-                        PlayableItem::Track(track) => Some(track.clone()),
-                        _ => None,
-                    }
-                } else {
-                    None
-                },
-            }).collect();
-    return tracks_from_playlist;
+    return get_all_playable_tracks(&spotify, &playlist.tracks).await;
 }
 
-async fn get_all_user_tracks(spotify: &ClientCredsSpotify) -> Vec<PlaylistTrack> {
+async fn get_all_user_tracks(spotify: &AuthCodeSpotify) -> Vec<PlaylistTrack> {
     let mut all_user_tracks: Vec<PlaylistTrack> = Vec::new();
     #[cfg(not(feature = "use_cached_results"))]
     {
@@ -245,6 +266,11 @@ async fn get_all_user_tracks(spotify: &ClientCredsSpotify) -> Vec<PlaylistTrack>
 }
 
 fn remove_tracks(tracks: &mut Vec<PlaylistTrack>, removal_list: &Vec<PlaylistTrack>) {
+    println!(
+        "tracks len: {} removal list len: {}",
+        tracks.len(),
+        removal_list.len()
+    );
     tracks.retain(|track| {
         !removal_list.iter().any(|removal_track| {
             track.track.as_ref().unwrap().id.as_ref().unwrap()
@@ -253,11 +279,37 @@ fn remove_tracks(tracks: &mut Vec<PlaylistTrack>, removal_list: &Vec<PlaylistTra
     });
 }
 
-fn remove_from_playlist(spotify: &ClientCredsSpotify, playlist_id: &PlaylistId, tracks: &Vec<PlaylistTrack>) {
+fn remove_from_playlist(
+    spotify: &AuthCodeSpotify,
+    playlist_id: &PlaylistId,
+    tracks: &Vec<PlaylistTrack>,
+) {
     // api calls to remove from the recent one hundred playlist
 }
 
-fn add_to_playlist(spotify: &ClientCredsSpotify, playlist_id: &PlaylistId, tracks: &Vec<PlaylistTrack>) {
-    let track_ids: Vec<rspotify::model::TrackId> = tracks.iter().map(|track| track.track.as_ref().unwrap().id.as_ref().unwrap().clone()).collect();
-    // spotify.playlist_add_items(playlist_id, track_ids); // TODO: here :)
+async fn add_to_playlist(
+    spotify: &AuthCodeSpotify,
+    playlist_id: &PlaylistId<'_>,
+    tracks: &Vec<PlaylistTrack>,
+) {
+    if tracks.len() == 0 {
+        return;
+    }
+
+    let track_ids: Vec<rspotify::model::PlayableId> = tracks
+        .iter()
+        .map(|track| PlayableId::Track(track.track.as_ref().unwrap().id.as_ref().unwrap().clone()))
+        .collect();
+
+    match spotify
+        .playlist_add_items(playlist_id.clone(), track_ids, None)
+        .await
+    {
+        Ok(result) => {
+            println!("tracks added to playlist successfully!");
+        }
+        Err(error) => {
+            dbg!(error);
+        }
+    }
 }
